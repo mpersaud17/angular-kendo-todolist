405c7f12451228fd4b7313d1488fb7aae879168c
MelissaPersaud <MelissaPersaud@ows.test.lab>
2024-08-14 13:08:12 -0400

    updates

--- src/app/components/to-do-items/to-do-items.component.ts
+++ src/app/components/to-do-items/to-do-items.component.ts
@@ -2,13 +2,20 @@ import { Component } from '@angular/core';
 import { TodoService } from '../../services/todo.service';
 import { CommonModule } from '@angular/common';
 import { Items } from '../../models/items.model';
+import { FormsModule } from '@angular/forms';
+import { DialogsModule } from '@progress/kendo-angular-dialog'; // Import DialogsModule
+import { LabelModule } from '@progress/kendo-angular-label';
+import { InputsModule } from '@progress/kendo-angular-inputs';
+import { dataCsvIcon } from '@progress/kendo-svg-icons';
+
+// import { ButtonsModule } from '@progress/kendo-angular-buttons'; // Import ButtonsModule
 
 
 
 @Component({
   selector: 'app-to-do-items',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, DialogsModule, LabelModule, InputsModule, FormsModule],
   templateUrl: './to-do-items.component.html',
   styleUrl: './to-do-items.component.scss'
 })
@@ -16,16 +23,36 @@ export class ToDoItemsComponent {
 
   // empty to do list array
   toDoList: Items[] = []
+  isAddTaskDialogOpen = false;
+  newTask: Items = { id: 0, taskName: '' }; // Model to hold the new task data
+  taskInput: string = '';
 
-  constructor(private itemService : TodoService){
+
+  constructor(private toDoService : TodoService){
     
   }
 
   ngOnInit(){
-    this.itemService.getJson().subscribe(data => {
-      this.toDoList = data;
-      console.log(data);
-    } );
+    this.toDoService.getItems()
+    .subscribe(items => this.toDoList = items);
+  }
+
+  // once the "Add Task" Button is clicked, this method is called and this sets the boolean value to true
+  openAddTaskDialog() {
+    this.isAddTaskDialogOpen = true;
+    console.log("boolean value is now " + this.isAddTaskDialogOpen)
+  }
+
+  closeAddTaskDialog() {
+    this.isAddTaskDialogOpen = false;
+    console.log("boolean value is now " + this.isAddTaskDialogOpen)
+  }
+
+  addTask() {
+    // will need to call the todoService in here to update the array with the newly added task
+    this.newTask.taskName=this.taskInput;
+    this.toDoService.addItems(this.newTask).subscribe(data => this.toDoList = data);
+    this.closeAddTaskDialog();
   }
 
 }
--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,18 +1,26 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { Items } from '../models/items.model';
+import data from '../../assets/db.json';
+import { Data } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
+  
+  // initialize a behaviorSubject with a type of Items array, and initialize it with the data from the db.json file.
+  private itemsSubject: BehaviorSubject<Items[]> = new BehaviorSubject(data);
 
+  // 
+  public getItems(): Observable<Items[]>{
+    return this.itemsSubject;
+  }
 
-  constructor(private httpClient: HttpClient) {}
-
-  public getJson(): Observable<Items[]>{
-    return this.httpClient.get<Items[]>("../assets/db.json");
+  public addItems(newTask: Items): Observable<Items[]> {
+    let  newArray = [...data, newTask];
+    this.itemsSubject.next(newArray);
+    return this.itemsSubject;
   }
 
 }
