486d49f8341adcfdfe1d1de9db0bbf2752497f0d
MelissaPersaud <MelissaPersaud@ows.test.lab>
2024-08-15 08:49:33 -0400

    refactor

--- src/app/components/to-do-list/to-do-list.component.ts
+++ src/app/components/to-do-list/to-do-list.component.ts
@@ -4,21 +4,32 @@ import { Observable } from 'rxjs';
 import { Item } from '../../models/item.model';
 import { TodoService } from '../../services/todo.service';
 import { CommonModule } from '@angular/common';
+import { DialogsModule } from '@progress/kendo-angular-dialog';
 
 
 @Component({
   selector: 'app-to-do-list',
   standalone: true,
-  imports: [CommonModule, ToDoItemComponent],
+  imports: [CommonModule, ToDoItemComponent, DialogsModule],
   templateUrl: './to-do-list.component.html',
   styleUrl: './to-do-list.component.scss'
 })
 export class ToDoListComponent {
+  isAddTaskDialogOpen: boolean = false;
   toDoList: Observable<Item[]>; // Observable to hold the list of items
 
   constructor(private todoService: TodoService) {
     this.toDoList = this.todoService.getItems(); // Directly assign the observable
-    this.toDoList.forEach(data=> console.log(data))
+  }
+
+  public openAddTaskDialog(): void {
+    this.isAddTaskDialogOpen = true;
+    console.log("boolean value is now " + this.isAddTaskDialogOpen)
+  }
+
+  public closeAddTaskDialog(): void {
+    this.isAddTaskDialogOpen = false;
+    console.log("boolean value is now " + this.isAddTaskDialogOpen)
   }
  
 
